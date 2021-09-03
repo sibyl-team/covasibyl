050cc6bda3b4f84c665e4ffd5fc0277a71430ed8
Fabio Mazza <fab4mazz@gmail.com>
2021-09-03 16:24:27 +0200

    Trial code for intervention

--- sibyl_covasim/rankquarantine.py
+++ sibyl_covasim/rankquarantine.py
@@ -1,37 +1,116 @@
+import numpy as np
 import scipy.sparse as sp
 import covasim.interventions as cvi
+import covasim.utils as cvu
+from .test import CovasimTester
 
 
 class Mitigation(cvi.Intervention):
     """
     Class to run epidemic mitigation
-    Give a ranker from epidemic_mitigation, and it will run the ranker and 
+    Give a ranker from epidemic_mitigation, and it will run the ranker and
     quarantine people
     """
 
-    def __init__(self, ranker, label):
+    def __init__(self,
+                ranker,
+                label,
+                n_tests, start_day=0, end_day=None,
+                sensitivity=1.0, specificity:float=1.0, 
+                loss_prob=0, test_delay=0,
+                quar_period=None,
+                notif_delay=0):
 
         super().__init__(label="Mitigation: "+label)
 
         self.ranker = ranker
+        self.n_daily_tests = n_tests
+        self.sensitivity = sensitivity
+        self.specificity = specificity
+        self.loss_prob   = loss_prob
+        self.test_delay  = test_delay
+        self.start_day   = start_day
+        self.end_day     = end_day
+        self.quar_period = quar_period
+        self.notif_delay = notif_delay
+
         self.contacts_day = None
         self.delayed_init = False
+        self.N = None
+        self.all_observ = None
+        self.daily_obs = None
+        self.ranker_data = None
+        self.tester = None
 
-    def _init_ranker(self, sim):
+    def _init_for_sim(self, sim):
         """
         initialize the ranker
         """
+        if self.quar_period is None:
+            self.quar_period = sim['quar_period']
+        
         pars = sim.pars
-        N = pars["pop_size"]
+        self.N = pars["pop_size"]
         T = pars["n_days"]
-        self.ranker.init(N, T)
+        self.ranker.init(self.N, T)
+
+        self.tester = CovasimTester(sim)
     
     def initialize(self, sim=None):
         super().initialize(sim)
         if sim is None:
             self.delayed_init = True
         else:
-            self._init_ranker(sim)
+            self._init_for_sim(sim)
+        
+        self.all_observ =[]
+        self.daily_obs = []
+        self.ranker_data = {}
+
 
     def apply(self, sim):
-        pass
+        
+        day = sim.t
+        if self.delayed_init:
+            self._init_for_sim(sim)
+            self.delayed_init = False
+
+        # get contacts of the day
+        conts_m = sum(
+            sp.csr_matrix(
+            (layer["beta"],(layer["p1"],layer["p2"])), shape=(self.N, self.N))
+            for k,layer in sim.people.contacts.items()
+            )
+        
+        conts_m = conts_m.tolil()
+        conts_m.setdiag(0.)
+        conts_m = conts_m.tocoo()
+
+        contacts_day = [(i,j,day, val) for i,j,val in zip(conts_m.row, conts_m.col, conts_m.data)]
+        
+        ### get rank from the algorithm
+        rank_algo = self.ranker.rank(day, contacts_day, self.daily_obs, self.ranker_data)
+        rank = np.array(sorted(rank_algo, key= lambda tup: tup[1], reverse=True))
+        rank = rank[:,0].astype(int)
+
+        test_inds = rank[:self.n_daily_tests]
+        ## test people
+        ## not using sim.people.test because doesn't record the susceptible tests
+        #sim.people.test(test_inds, test_sensitivity=self.sensitivity, loss_prob=self.loss_prob, test_delay=self.test_delay)
+
+        self.tester.test(sim, test_inds,
+                    test_sensitivity=self.sensitivity,
+                    test_specificity=self.specificity,
+                    loss_prob=self.loss_prob, test_delay=self.test_delay)
+
+        ## find people who are tested today
+        ## date_tested is the date in which the test has been required
+        results_day = self.tester.get_results(day)
+        ## change obs
+        self.daily_obs = [(idx, st, day) for idx, st in results_day]
+
+        ## quarantine individuals
+        inds_quar = cvu.true(sim.people.date_diagnosed == sim.t)
+        sim.people.schedule_quarantine(inds_quar,
+                    start_date=sim.t + self.notif_delay,
+                    period=self.quar_period - self.notif_delay)
---
+++ sibyl_covasim/test.py
@@ -0,0 +1,77 @@
+import numpy as np
+import covasim.interventions as cvi
+import covasim.utils as cvu
+
+# TODO: make class to do tests, with delay
+
+class CovasimTester:
+
+    def __init__(self, sim):
+        
+        pars = sim.pars
+        self.N = pars["pop_size"]
+        self.date_diagn_state = np.full((3,self.N), np.nan)
+
+    def test(self, sim, inds, test_sensitivity=1.0, test_specificity=1.0, loss_prob=0.0, test_delay=0):
+        '''
+        Method to test people, accounting for recovered 
+        and susceptible individuals (SIR model)
+
+
+        Args:
+            inds: indices of who to test
+            test_sensitivity (float): probability of a true positive
+            test_specificity (float): probability of a true negative
+            loss_prob (float): probability of loss to follow-up
+            test_delay (int): number of days before test results are ready
+        '''
+        people = sim.people
+        inds = np.unique(inds)
+        
+        people.tested[inds] = True
+        people.date_tested[inds] = sim.t # Only keep the last time they tested
+
+        ## lost tests
+        not_lost = cvu.n_binomial(1.0-loss_prob, len(inds))
+        inds_test = inds[not_lost]
+
+        ## check susceptibles
+        susc_inds = cvu.itruei(people.susceptible, inds_test)
+        neg_test = cvu.n_binomial(test_specificity, len(susc_inds))
+        test_susc = susc_inds[neg_test]
+        test_inf = susc_inds[np.logical_not(neg_test)]
+
+        ### find infectious indices
+        is_infectious = cvu.itruei(people.infectious, inds_test)
+        ## find the ones which test positive
+        pos_test      = cvu.n_binomial(test_sensitivity, len(is_infectious))
+        is_inf_pos    = is_infectious[pos_test]
+        ### concatenate results
+        test_susc = np.concatenate((test_susc, is_infectious[np.logical_not(pos_test)]))
+        test_inf = np.concatenate((test_inf, is_inf_pos))
+
+        ## infectious, positive, not diagnosed yet
+        inf_not_diagnosed = test_inf[np.isnan(people.date_diagnosed[test_inf])]
+        #not_lost      = cvu.n_binomial(1.0-loss_prob, len(not_diagnosed))
+        #final_inds    = not_diagnosed[not_lost]
+
+        # check for recovered
+        rec_inds = cvu.itruei(people.recovered, inds_test)
+        # TODO: use specifity here?
+        ## check they haven't been diagnosed before?
+        test_rec = rec_inds#rec_inds[np.isnan(people.date_diagnosed[rec_inds])]
+        
+        ## Keep this for compatibility
+        # Store the date the person will be diagnosed, as well as the date they took the test which will come back positive
+        people.date_diagnosed[inf_not_diagnosed] = sim.t + test_delay
+        people.date_pos_test[inf_not_diagnosed] = sim.t
+
+        self.date_diagn_state[0, test_susc] = sim.t + test_delay
+        self.date_diagn_state[1, inf_not_diagnosed] = sim.t + test_delay
+        self.date_diagn_state[2, test_rec] = sim.t + test_delay
+
+        return
+
+    def get_results(self, day):
+
+        return np.stack(np.where(self.date_diagn_state == day),1)[:,::-1]
