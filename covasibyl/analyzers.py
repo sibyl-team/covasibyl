318c31086110faa542f59b06ae64fb6a6c8fc51d
Fabio Mazza <fab4mazz@gmail.com>
2022-08-05 17:56:04 +0200

    add analyzer for quarantine

--- covasibyl/analyzers.py
+++ covasibyl/analyzers.py
@@ -1,8 +1,13 @@
+from collections import defaultdict
+import warnings
+
 import numpy as np
 import matplotlib.pyplot as pl
 
 import sciris as sc
 from covasim.analysis import Analyzer
+from covasibyl import utils
+import covasim.utils as cvu
 
 
 class store_seir(Analyzer):
@@ -53,3 +58,80 @@ class store_seir(Analyzer):
         
         return np.array(list(zip(self.t, self.S, self.E, self.I, self.R, self.Q, self.IND, self.Efree)),
             dtype=[(i,np.int_) for i in ["t","S","E","I","R", "Q", "nondiag","Enondiag"]])
+
+
+class QuarantineSaver(Analyzer):
+    def __init__(self, printout=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        self.quar_day = dict()
+
+        self.dequar_day = dict()
+
+        self.inquar = None
+
+    def apply(self, sim):
+
+        day = sim.t
+
+        quar_idcs = np.where(sim.people.quarantined)[0]
+
+        if self.inquar is None:
+            self.quar_day[day] = quar_idcs
+            ## no going out of quarantine
+        else:
+            new_quar = np.setdiff1d(quar_idcs, self.inquar)
+            self.quar_day[day] = new_quar
+            new_out = np.setdiff1d(self.inquar, quar_idcs)
+            self.dequar_day[day] = new_out
+
+        self.inquar = quar_idcs
+
+        pending = sim.people._pending_quarantine
+        if len(pending[day]) > 0:
+            pd_q = np.array(pending[day])
+            idcs_pending = pd_q[:,0]
+            notq = np.setdiff1d(idcs_pending, self.quar_day[day])
+            iquar = cvu.itruei(sim.people.quarantined | sim.people.diagnosed | sim.people.dead | sim.people.recovered, notq)
+            notq = np.setdiff1d(notq, iquar)
+            #iddiag = cvu.itruei(sim.people.diagnosed, notq)
+            #notq = np.setdiff1d(notq, iddiag)
+            if len(notq) > 0:
+                print(f"Day {day}, Not quarantining idcs {notq}")
+        else:
+            assert len(self.quar_day[day]) == 0
+
+class ContactsSaver(Analyzer):
+    def __init__(self,quar_factor=1., printout=False, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+    
+        self.contacts_saved = None
+        self.quar_factor=quar_factor
+    
+        self._warned = defaultdict(lambda: False)
+
+    def _warn_once(self, key:str, message:str):
+        if not self._warned[key]:
+            warnings.warn(message)
+            self._warned[key] = True
+
+    def apply(self, sim):
+
+        N = len(sim.people.sex)
+        contacts_df = utils.get_contacts_day(sim.people)
+
+        tested_iso = cvu.true(sim.people.diagnosed)
+        contacts_df = utils.filt_contacts_df(contacts_df,
+                tested_iso, self.iso_cts_strength, N, only_i=True)
+
+        if self.quar_factor < 1:
+            ## filter the contacts for quarantined
+            self._warn_once("qfact", f"Quarantine factor is {self.quar_factor}")
+            quar_idcs = cvu.true(sim.people.quarantined)
+
+            if len(quar_idcs) > 0:
+                msum = contacts_df["m"].sum()
+                contacts_df = utils.filt_contacts_df(contacts_df, quar_idcs, self.quar_factor, N)
+                assert conts_prev_day["m"].sum() < msum
+
+        self.contacts_saved = contacts_df
